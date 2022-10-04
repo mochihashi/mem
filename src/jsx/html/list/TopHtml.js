fa7185517f630e7d02dfc8b7297af46b5994b81c
mochihashi <h.mochihashi@biztech-fusion.com>
2022-10-04 13:10:44 +0900

    top categories

--- src/jsx/html/list/TopHtml.js
+++ src/jsx/html/list/TopHtml.js
@@ -2,25 +2,31 @@
 import CardListHtml from 'html/common/CardListHtml';
 
 export default function() {
-	let links = '';
+	let allTables = '', allCategories = '';
 	window.app.readJson('data/top/', function(data){
-		links += CardListHtml({list: sortByIdDesc(data.tables)});
-		links += CardListHtml({list: sortByIdDesc(data.categories)});
-		if(!links) links = `<span class="lang-msg-no-data"></span>`;
-		$('#row-tables').html(links);
+		allTables = CardListHtml({list: sortByIdDesc(data.tables)});
+		allCategories = CardListHtml({list: sortByIdDesc(data.categories)});
+		if(!allTables) allTables = `<span class="lang-msg-no-data"></span>`;
+		if(!allCategories) allCategories = `<span class="lang-msg-no-data"></span>`;
+		$('#row-tables').html(allTables);
+		$('#row-categories').html(allCategories);
 	});
 	
 	$('#search-text').change(function(){
 		let q = $('#search-text').val();
 		if(!q) {
-			$('#row-tables').html(links);
+			$('#row-tables').html(allTables);
+			$('#row-categories').html(allCategories);
 		} else {
 			window.app.readJson('api/top/search/', function(data){
-				let html= '';
-				html += CardListHtml({list: data.tables});
-				html += CardListHtml({list: data.categories});
+				let html = '';
+				html = CardListHtml({list: data.tables});
 				if(!html) html = `<span class="lang-msg-no-data"></span>`;
 				$('#row-tables').html(html);
+
+				html = CardListHtml({list: data.categories});
+				if(!html) html = `<span class="lang-msg-no-data"></span>`;
+				$('#row-categories').html(html);
 			}, {q: q});
 		}
 	});
